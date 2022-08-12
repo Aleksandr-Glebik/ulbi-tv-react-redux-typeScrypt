5ffb16d3b8c71c3906aba36583bdf4d4a0b81b1b
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-12 20:17:00 +0300

    feat: add todoReducer, todo types, connect reducer in combineReducers

---
+++ r-r-ts/src/store/reducers/todoReducer.ts
@@ -0,0 +1,25 @@
+import { TodoAction, TodoActionTypes } from './../../types/todo';
+import { TodoState } from "../../types/todo"
+
+const initialState: TodoState = {
+    todos: [],
+    loading: false,
+    page: 1,
+    limit: 10,
+    error: null,
+}
+
+export const todoReducer = (state = initialState, action: TodoAction): TodoState => {
+    switch (action.type) {
+        case TodoActionTypes.FETCH_TODOS:
+            return {...state, loading: true}
+        case TodoActionTypes.FETCH_TODOS_SUCCESS:
+            return {...state, loading: false, todos: action.payload}
+        case TodoActionTypes.FETCH_TODOS_ERROR:
+            return {...state, loading: false, error: action.payload}
+        case TodoActionTypes.SET_TODOS_PAGE:
+            return {...state, page: action.payload}
+        default:
+            return state
+    }
+}
