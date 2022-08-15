e5f4db088ef2a9d25482040f1a40f8d0b76c83bf
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-15 12:47:15 +0300

    feat: add pagination for todoList

--- r-r-ts/src/components/todoList.tsx
+++ r-r-ts/src/components/todoList.tsx
@@ -4,11 +4,12 @@ import { useTypedSelector } from "../hooks/useTypedSelector"
 
 const TodoList: React.FC = () => {
     const {todos, loading, page, limit, error} = useTypedSelector(state => state.todo)
-    const {fetchTodos} = useActions()
+    const {fetchTodos, setTodoPage} = useActions()
+    const pages = [1, 2, 3, 4, 5]
 
     useEffect( () => {
         fetchTodos(page, limit)
-    }, [])
+    }, [page])
 
     if (loading) {
         return <h1>Идет загрузка...</h1>
@@ -24,6 +25,20 @@ const TodoList: React.FC = () => {
             {todos.map(todo =>
                 <div key={todo.id}>{todo.id} - {todo.title}</div>
             )}
+            <div style={{display: 'flex'}}>
+                {pages.map( p =>
+                    <div
+                        onClick={() => setTodoPage(p)}
+                        style={{padding: '10px',
+                        border: p === page
+                            ? '2px solid green'
+                            : '1px solid gray'
+                    }}>
+                        {p}
+                    </div>
+                )}
+            </div>
+
         </div>
     )
 }
